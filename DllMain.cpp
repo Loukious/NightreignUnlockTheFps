70faa28c56e1f2b579ec885535ca859affbb6668
Loukious <Loukious@users.noreply.github.com>
2025-05-31 15:09:08 +0100

    Intial commit

---
+++ DllMain.cpp
@@ -0,0 +1,114 @@
+#include <Windows.h>
+#include <algorithm>
+
+#include "ModUtils.h"
+
+using namespace ModUtils;
+using namespace mINI;
+
+static float fpsLimit = 300;
+
+void ReadConfig()
+{
+	INIFile config(GetModFolderPath() + "\\config.ini");
+	INIStructure ini;
+
+	if (config.read(ini))
+	{
+		fpsLimit = std::stof(ini["unlockthefps"].get("limit"));
+	}
+	else
+	{
+		ini["unlockthefps"]["limit"] = "300";
+		config.write(ini, true);
+	}
+
+	Log("FPS limit: ", fpsLimit);
+}
+
+DWORD WINAPI MainThread(LPVOID lpParam)
+{
+	Log("Activating UnlockTheFps...");
+	{
+		ReadConfig();
+
+		const std::string PATTERN_UNLOCK_FPS = "c7 ? ? ? 88 88 3c eb";
+		const int PATTERN_UNLOCK_FPS_OFFSET = 3;
+
+		const std::string EXPECTED_UNLOCK_FPS_BYTES = "89 88 88 3c";
+		std::string patchedUnlockFpsBytes = "90 90 90 90";
+
+		float frametime = (1000.0f / fpsLimit) / 1000.0f;
+		Log("Frametime: ", frametime);
+
+		std::vector<unsigned char> frametimeBytes(sizeof(float), 0);
+		MemCopy((uintptr_t)&frametimeBytes[0], (uintptr_t)&frametime, sizeof(float));
+		patchedUnlockFpsBytes = RawAobToStringAob(frametimeBytes);
+
+		uintptr_t patchAddress = AobScan(PATTERN_UNLOCK_FPS);
+		if (patchAddress == 0)
+		{
+			Log("UnlockTheFps pattern not found!");
+			return 1;
+		}
+
+		patchAddress += PATTERN_UNLOCK_FPS_OFFSET;
+
+		if (!ReplaceExpectedBytesAtAddress(patchAddress, EXPECTED_UNLOCK_FPS_BYTES, patchedUnlockFpsBytes))
+		{
+			Log("Failed to patch UnlockTheFps at address: 0x%p", (void*)patchAddress);
+			return 1;
+		}
+	}
+
+	Log("Removing 60 FPS fullscreen limit...");
+	{
+		const std::string PATTERN_HERTZLOCK = "eb ? c7 ? ? 3c 00 00 00 c7 ? ? 01 00 00 00";
+		const int PATTERN_HERTZLOCK_OFFSET = 2;
+		const int PATTERN_HERTZLOCK_OFFSET_INTEGER1 = 3;
+		const int PATTERN_HERTZLOCK_OFFSET_INTEGER2 = 10;
+
+		const std::string EXPECTED_FPS_BYTES = "3c 00 00 00";
+		const std::string PATCHED_FPS_BYTES = "00 00 00 00";
+
+		const std::string EXPECTED_FLAG_BYTES = "01 00 00 00";
+		const std::string PATCHED_FLAG_BYTES = "00 00 00 00";
+
+		uintptr_t patchAddress = AobScan(PATTERN_HERTZLOCK);
+		if (patchAddress == 0)
+		{
+			Log("Pattern not found!");
+			return 1;
+		}
+		patchAddress += PATTERN_HERTZLOCK_OFFSET;
+
+		uintptr_t fpsValueAddr = patchAddress + PATTERN_HERTZLOCK_OFFSET_INTEGER1;
+		uintptr_t flagAddr = patchAddress + PATTERN_HERTZLOCK_OFFSET_INTEGER2;
+
+		if (!ReplaceExpectedBytesAtAddress(fpsValueAddr, EXPECTED_FPS_BYTES, PATCHED_FPS_BYTES))
+		{
+			Log("Failed to patch FPS limit at address: 0x%p", (void*)fpsValueAddr);
+			return 1;
+		}
+
+		if (!ReplaceExpectedBytesAtAddress(flagAddr, EXPECTED_FLAG_BYTES, PATCHED_FLAG_BYTES))
+		{
+			Log("Failed to patch refresh rate flag at address: 0x%p", (void*)flagAddr);
+			return 1;
+		}
+		Log("Successfully removed 60 FPS limit!");
+	}
+
+	CloseLog();
+	return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE module, DWORD reason, LPVOID)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		DisableThreadLibraryCalls(module);
+		CreateThread(0, 0, &MainThread, 0, 0, NULL);
+	}
+	return 1;
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+## UnlockTheFps for Elden Ring Nightreign
+This is a fork of [UnlockTheFps](https://github.com/techiew/EldenRingMods/blob/master/UnlockTheFps) modified for Nightreign.
+
+## Features
+- Unlocks the framerate in Elden Ring Nightreign.
+- Unlocks the monitor refresh rate.
+- Supports Seamless Co-op.
+
+## Requirements
+- [Elden Ring Mod Loader](https://github.com/techiew/EldenRingModLoader/releases/latest) installed.
+- EAC (Easy Anti-Cheat) must be disabled.
+
+## Installation
+1. Download the latest release of this mod from the [releases page](./releases/latest)
+2. Extract the contents of the downloaded zip file into your `mods` folder, which is located in the same directory as your `nightreign.exe`.
+3. Launch the game as normal.
+
+## Configuration
+- You can configure a custom max FPS by editing the `config.ini` file located in the `mods/UnlockTheFps` directory.
+
+## Credits
+- [techiew](https://github.com/techiew) for the original UnlockTheFps mod.
+
+## Support the fork developer
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://buymeacoffee.com/loukious)
